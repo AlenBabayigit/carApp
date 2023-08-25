3eb3fbb16388117810ca80cfe4596f7a6a988494
Alen Babayigit <alenbabayigit@gmail.com>
2023-08-25 08:00:35 +0300

    Add exceptions and exception handlers

---
+++ src/main/java/com/alenbabayigit/carApp/exception/BusinessException.java
@@ -0,0 +1,8 @@
+package com.alenbabayigit.carApp.exception;
+
+public class BusinessException extends RuntimeException {
+
+    public BusinessException(String message) {
+        super(message);
+    }
+}
---
+++ src/main/java/com/alenbabayigit/carApp/exception/GlobalExceptionHandler.java
@@ -0,0 +1,19 @@
+package com.alenbabayigit.carApp.exception;
+
+import com.alenbabayigit.carApp.util.ResponseBuilder;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler
+    @ResponseStatus(code = HttpStatus.BAD_REQUEST)
+    public ResponseEntity<?> handleBusinessException(BusinessException exception) {
+        return ResponseBuilder.error(exception.getMessage());
+    }
+
+}
