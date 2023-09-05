4e7f3cc3b24246188ab1596681d9cf37baecf6f8
Alen Babayigit <alenbabayigit@gmail.com>
2023-09-05 07:11:40 +0300

    Add DAOs for User subclasses

---
+++ src/main/java/com/alenbabayigit/carApp/user/CorporateCustomerRepository.java
@@ -0,0 +1,5 @@
+package com.alenbabayigit.carApp.user;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CorporateCustomerRepository extends JpaRepository<CorporateCustomer, Integer> {}
---
+++ src/main/java/com/alenbabayigit/carApp/user/IndividualCustomerRepository.java
@@ -0,0 +1,7 @@
+package com.alenbabayigit.carApp.user;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface IndividualCustomerRepository extends JpaRepository<IndividualCustomer, Integer> {
+
+}
