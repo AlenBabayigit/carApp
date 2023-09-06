395e4bd1af897ed434128a502bab0c06d5fbd3f5
Alen Babayigit <alenbabayigit@gmail.com>
2023-09-07 01:52:55 +0300

    Add jpa methods and service implementation for CorporateCustomer

--- src/main/java/com/alenbabayigit/carApp/corporatecustomer/CorporateCustomerRepository.java
+++ src/main/java/com/alenbabayigit/carApp/corporatecustomer/CorporateCustomerRepository.java
@@ -2,4 +2,8 @@ package com.alenbabayigit.carApp.corporatecustomer;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface CorporateCustomerRepository extends JpaRepository<CorporateCustomer, Integer> {}
+public interface CorporateCustomerRepository extends JpaRepository<CorporateCustomer, Integer> {
+    boolean existsCorporateCustomerByEmailIgnoreCase(String email);
+    boolean existsCorporateCustomerByPhoneNumber(String phoneNumber);
+    boolean existsCorporateCustomerByTaxNumber(String taxNumber);
+}
--- src/main/java/com/alenbabayigit/carApp/corporatecustomer/CorporateCustomerServiceImpl.java
+++ src/main/java/com/alenbabayigit/carApp/corporatecustomer/CorporateCustomerServiceImpl.java
@@ -2,39 +2,84 @@ package com.alenbabayigit.carApp.corporatecustomer;
 
 import com.alenbabayigit.carApp.corporatecustomer.model.request.CreateCorporateCustomerRequest;
 import com.alenbabayigit.carApp.corporatecustomer.model.request.UpdateCorporateCustomerRequest;
+import com.alenbabayigit.carApp.corporatecustomer.model.response.CorporateCustomerGetAllResponse;
 import com.alenbabayigit.carApp.corporatecustomer.model.response.CorporateCustomerGetByIdResponse;
+import com.alenbabayigit.carApp.exception.BusinessException;
+import com.alenbabayigit.carApp.util.ResponseBuilder;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class CorporateCustomerServiceImpl implements CorporateCustomerService {
+
+    private final CorporateCustomerRepository corporateCustomerRepository;
+
+    public CorporateCustomerServiceImpl(CorporateCustomerRepository corporateCustomerRepository) {
+        this.corporateCustomerRepository = corporateCustomerRepository;
+    }
+
     @Override
     public CorporateCustomer create(CreateCorporateCustomerRequest createCorporateCustomerRequest) {
-        return null;
+        checkCorporateCustomerAlreadyExists(createCorporateCustomerRequest.email(), createCorporateCustomerRequest.phoneNumber(), createCorporateCustomerRequest.taxNumber());
+        CorporateCustomer corporateCustomer = new CorporateCustomer();
+        corporateCustomer.setEmail(createCorporateCustomerRequest.email());
+        corporateCustomer.setPassword(createCorporateCustomerRequest.password());
+        corporateCustomer.setPhoneNumber(createCorporateCustomerRequest.phoneNumber());
+        corporateCustomer.setCompanyName(createCorporateCustomerRequest.companyName());
+        corporateCustomer.setTaxNumber(createCorporateCustomerRequest.taxNumber());
+        return corporateCustomerRepository.save(corporateCustomer);
+    }
+
+    public void checkCorporateCustomerAlreadyExists(String email, String phoneNumber, String taxNumber) {
+        boolean isEmailExists = corporateCustomerRepository.existsCorporateCustomerByEmailIgnoreCase(email);
+        boolean isPhoneNumberExists = corporateCustomerRepository.existsCorporateCustomerByPhoneNumber(phoneNumber);
+        boolean isTaxNumberExists = corporateCustomerRepository.existsCorporateCustomerByTaxNumber(taxNumber);
+        if (isEmailExists || isPhoneNumberExists || isTaxNumberExists) {
+            throw new BusinessException("Corporate customer already exists with following email, phone number & tax number: "
+                    + email + ", " + phoneNumber + ", " + taxNumber);
+        }
+
+
     }
 
     @Override
     public CorporateCustomerGetByIdResponse getById(Integer id) {
-        return null;
+        CorporateCustomer corporateCustomer = getCorporateCustomerById(id);
+        return new CorporateCustomerGetByIdResponse(corporateCustomer.getEmail(), corporateCustomer.getPassword(),
+            corporateCustomer.getPhoneNumber(), corporateCustomer.getCompanyName(), corporateCustomer.getTaxNumber());
     }
 
     @Override
     public ResponseEntity<?> getAll() {
-        return null;
+        List<CorporateCustomerGetAllResponse> list = corporateCustomerRepository.findAll().stream().map(corporateCustomer -> new CorporateCustomerGetAllResponse(corporateCustomer.getId(), corporateCustomer.getEmail(), corporateCustomer.getPassword(), corporateCustomer.getPhoneNumber(), corporateCustomer.getCompanyName(), corporateCustomer.getTaxNumber())).toList();
+        return ResponseBuilder.success("Data listed successfully.", list);
     }
 
     @Override
     public CorporateCustomer update(Integer id, UpdateCorporateCustomerRequest updateCorporateCustomerRequest) {
-        return null;
+        checkCorporateCustomerAlreadyExists(
+            updateCorporateCustomerRequest.email(),
+            updateCorporateCustomerRequest.phoneNumber(),
+            updateCorporateCustomerRequest.taxNumber());
+        CorporateCustomer corporateCustomer = getCorporateCustomerById(id);
+        corporateCustomer.setEmail(updateCorporateCustomerRequest.email());
+        corporateCustomer.setPassword(updateCorporateCustomerRequest.password());
+        corporateCustomer.setPhoneNumber(updateCorporateCustomerRequest.phoneNumber());
+        corporateCustomer.setCompanyName(updateCorporateCustomerRequest.companyName());
+        corporateCustomer.setTaxNumber(updateCorporateCustomerRequest.taxNumber());
+        return corporateCustomerRepository.save(corporateCustomer);
     }
 
     @Override
     public CorporateCustomer getCorporateCustomerById(Integer id) {
-        return null;
+        return corporateCustomerRepository.findById(id).orElseThrow(() -> new BusinessException("There is no corporate customer with following id: " + id));
     }
 
     @Override
     public void delete(Integer id) {
-
+        getCorporateCustomerById(id);
+        corporateCustomerRepository.deleteById(id);
     }
 }
