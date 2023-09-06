3b32776d37bb8d89d788f5d7f07475c335b7eaf2
Alen Babayigit <alenbabayigit@gmail.com>
2023-09-07 01:55:08 +0300

    Add @Repository annotation to CorporateCustomerRepository

--- src/main/java/com/alenbabayigit/carApp/corporatecustomer/CorporateCustomerRepository.java
+++ src/main/java/com/alenbabayigit/carApp/corporatecustomer/CorporateCustomerRepository.java
@@ -1,7 +1,9 @@
 package com.alenbabayigit.carApp.corporatecustomer;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface CorporateCustomerRepository extends JpaRepository<CorporateCustomer, Integer> {
     boolean existsCorporateCustomerByEmailIgnoreCase(String email);
     boolean existsCorporateCustomerByPhoneNumber(String phoneNumber);
